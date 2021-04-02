138813793e620a57a8623584554c89144bed64b7
Mohamed-Nady13 <80857714+Mohamed-Nady13@users.noreply.github.com>
2021-04-03 00:03:46 +0200

    server code

--- server.js
+++ server.js
@@ -2,18 +2,39 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+//const send = require("send");
+app.use(cors());
 
 // Initialize the main project folder
+
 app.use(express.static('website'));
 
 
 // Setup Server
+
+const port = 8888;
+const server = app.listen(port, function() {
+    console.log("server Started, Hi there !");
+});
+app.get("/getdata", (req, res) => {
+    res.send(projectData);
+})
+app.post("/savedate", (req, res) => {
+    projectData.temp = req.body.temp;
+    projectData.date = req.body.date;
+    projectData.feelings = req.body.feelings;
+})
