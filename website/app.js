d98e80fb429d3ac3fba76551b9636e8ac34cc456
Mohamed-Nady13 <80857714+Mohamed-Nady13@users.noreply.github.com>
2021-04-03 00:02:28 +0200

    Server side code

--- website/app.js
+++ website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
 
+const apiKey = "e739516ac81ed980cc7511954ada8827";
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
